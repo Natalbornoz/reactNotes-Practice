0c7915c72c624ba5ed59e3353a45f7677fa86f37
Natalbornoz <natalbornoz@gmail.com>
2018-03-15 13:48:57 -0300

    Create components and add styles

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      // Nombre contenedor de todo el proyecto y las secciones del sitio
+      <div className="notesContainer">
+        
+        <div className="notesHeader">
+          <h1>React y Firebase App</h1>
+        </div>
+        <div className="notesBody">
+        </div>
+        <div className="notesFooter">
+        </div>
       </div>
     );
   }
