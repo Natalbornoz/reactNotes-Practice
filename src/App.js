b4ef65bb83b8e31190752eef29f02bc64bb2a428
Natalbornoz <natalbornoz@gmail.com>
2018-03-15 14:25:14 -0300

    function add notes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Note from './Note/Note';
+import NoteForm from './NoteForm/NoteForm';
+
 class App extends Component {
+
+  //Para poder mostrar notas en estado
+  constructor() {
+    super();
+    //Almacenará datos de nuestra aplicación
+    this.state = {
+      notes: [
+        {noteId: 1, noteContent: 'note 1'},
+        {noteId: 2, noteContent: 'note 2'}
+      ]
+    };
+    this.addNote = this.addNote.bind(this);
+  }
+
+  removeNote() {
+
+  }
+
+  // para guardar notas
+  addNote(note) {
+    // obten las notas desde el estado
+    let { notes } = this.state;
+    notes.push({
+      noteId: notes.lenght + 1,
+      noteContent: note
+  });
+  // Para actualizar estado
+  this.setState({ notes });
+
+}
   render() {
     return (
       // Nombre contenedor de todo el proyecto y las secciones del sitio
       <div className="notesContainer">
         
-        <div className="notesHeader">
-          <h1>React y Firebase App</h1>
-        </div>
-        <div className="notesBody">
-        </div>
-        <div className="notesFooter">
-        </div>
+          <div className="notesHeader">
+            <h1>React y Firebase App</h1>
+          </div>
+          {/* Cuerpo de la aplicacion */}
+          <div className="notesBody">
+            {/* Desde el inicial , quiero recorrer las notas a traves de map */}
+            <ul>
+            {
+              this.state.notes.map(note => {
+                return (
+                  <Note
+                  noteContent ={note.noteContent}
+                  noteId={note.noteId}
+                  key={note.noteId}
+                  />
+                )
+              })
+            }
+            </ul> 
+          </div>
+
+          {/* Acá irá el formulario */}
+          <div className="notesFooter">
+            <NoteForm addNote={this.addNote}/>
+          </div>
       </div>
     );
   }
---
+++ src/Note/Note.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './Note.css';
+
+class Note extends Component {
+    constructor(props) {
+        super(props);
+        this.noteContent = props.noteContent;
+        this.noteId = props.noteId;
+    }
+
+    handleRemove(id) {
+        console.log(id);
+    }
+
+ 
+
+    render() {
+       return (
+           <div className="Note">
+           <span
+                   onClick={() => this.handleRemove(this.noteId)} //   funicion  para eliminar elemento 
+           >&times;</span>
+               <p>{this.noteContent}</p>
+           </div>
+       )
+    }
+}
+
+export default Note;
